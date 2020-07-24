0c72fc4e1f4980b542e23b960a86465ec804891b
huanggengzhong <915835528@qq.com>
2020-07-24 22:48:56 +0800

    备份

--- 03-Class.dart
+++ 03-Class.dart
@@ -37,10 +37,48 @@
 //   print(class05.b);//'d'
 // }
 
+// class Class04{
+//   // 用final修饰常量
+//   final int a;
+//   final String b;
+//   // 用const 修饰构造函数
+//   const Class04(this.a,this.b);
+//   //普通函数
+//   void fun01(){
+//     print("aa");
+//   }
+// }
+
+// void main (){
+//   var class04=const Class04(1, "a");
+//   print(class04.a);//1
+//   class04.fun01();//aa
+
+// }
 
 
-
+class Class05{
+  int a;
+  String b;
+  Class05(int a,String b){
+    this.a=a;
+    this.b=b;
+  }
+  Class05.fun1(int a){
+    this.a=a;
+  }
+  // 重定向构造函数
+  Class05.fun2(String b):this.fun1(33);//重定向到函数fun1
+  Class05.fun3(String b):this(33,"ccc");//重定向到构造函数Class05
+}
 
 void main (){
+ var class05 =new Class05(55, "fff");
+ print(class05.a);//55
+ var class06=new Class05.fun2("hhh");
+ print(class06.a);//33,被重定向到了fun1
+ var class07=new Class05.fun3("hhh");
+ print(class07.a);//33,被重定向到了构造函数Class05
+ print(class07.b);//ccc,被重定向到了构造函数Class05
 
 }
